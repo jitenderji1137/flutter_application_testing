e18bb9712f5c8b88d63fa40122c3778e38f9c3a2
jitenderji1137 <trademetrader1137@gmail.com>
2023-12-05 16:58:06 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_testing/Home/HomePage.dart';
 
 void main() {
   runApp(FreeNetflix());
 }
 
 class FreeNetflix extends StatelessWidget {
-  const FreeNetflix({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Gadar'),
-          ),
-        )
-      )
+      home: HomePage(),
     );
   }
 }
